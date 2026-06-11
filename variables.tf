03dcc6dbc776fd821228f958b21d5ae79ea72f32
sonerg <sonerg@gmail.com>
2026-06-11 09:55:09 +0100

    alb_https_config

--- variables.tf
+++ variables.tf
@@ -37,10 +37,34 @@ variable "web_subnets" {
   }))
 }
 
+variable "alb_enable_https" {
+  description = "Whether to create an HTTPS listener on the ALB."
+  type        = bool
+  default     = false
+}
+
 variable "database_subnets" {
   description = "Private database subnets keyed by stable names."
   type = map(object({
     cidr = string
     az   = string
   }))
+}
+
+variable "alb_redirect_http_to_https" {
+  description = "Whether HTTP should redirect to HTTPS."
+  type        = bool
+  default     = false
+}
+
+variable "alb_certificate_arn" {
+  description = "ACM certificate ARN used by the ALB HTTPS listener."
+  type        = string
+  default     = null
+}
+
+variable "alb_ssl_policy" {
+  description = "SSL policy used by the ALB HTTPS listener."
+  type        = string
+  default     = "ELBSecurityPolicy-TLS13-1-2-2021-06"
 }
